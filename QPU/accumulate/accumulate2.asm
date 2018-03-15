40ce4bb9075f72fcb5ae7c45e6d07be45833f1f6
Jason Li <li07.jason@gmail.com>
2018-03-15 00:02:34 -0700

    Now sums correctly.

--- QPU/accumulate/accumulate2.asm
+++ QPU/accumulate/accumulate2.asm
@@ -1,75 +1,74 @@
 #reads first uniform into r3 (ignores it), moves ELEMENT_NUMBER into r1
 mov r3, ra32
 
-#reads second uniform into r2 (address of inputs)
-mov r0, ra32
 #shifts ELEMENT_NUMBER so it represents index of floats, not bytes
 shl r1, ra38, 2
-#adds ELEMENT_NUMBER*4 so that it fetches seperate inputs
-add r0, r0, r1
-#loads addresses to TMU
-mov ra56, r0
-ldtmu0
-mov r2, r4
-mov r1, 10000.0
-mov r0, 1000.0
-sub.setf ra39, r2, r1
-fadd.ifnn r2, r2, r0
+#reads second uniform into r2 (address of inputs)
+add r0, ra32, r1
+mov r1, 16
+shl ra1, r1, 2
+mov ra2, r1
 
-
-
-
-#add from 1 to 100
-mov r0, 0
-mov r1, 1.0
-mov r2, 99
-mov r3, 0.0
-:loop
-	add r0, r0, 1
-	sub.setf ra39, r2, r0
-	brr.allnn -, :loop
-	fadd r3, r3, r1
-	fadd r1, r1, 1.0
+#ra0 is accumulator
+#ra1 is ptr increment
+#ra2 is loop counter increment
+#r0 is pointer
+#r1 is limit
+#r2 is the addend
+#r3 is loop counter
+mov ra0, 0.0
+#read limit from third uniform
+sub r1, ra32, 1
+mov r3, ra38; mov r2, 0.0
+:accumulate_loop
+	fadd.ifnn ra0, ra0, r2
+	sub.setf ra39, r1, r3
+	mov ra56, r0
+	ldtmu0
+	mov r2, r4
+	add r0, r0, ra1
+	add r3, r3, ra2
+	brr.anynn -, :accumulate_loop
 	nop
-#ftoi ra0, r3
-mov ra0, r3
+	nop
+	nop
 
-#load element numbers into r0
-mov r0, ra38
+#load vector sums
+mov r0, ra0
 #clear r2
-mov r2, 0
+mov r2, 0.0
 #sum them
-add r2, r2, r0
+fadd r2, r2, r0
 mov r1, r0 << 1
-add r2, r2, r1
+fadd r2, r2, r1
 mov r0, r1 << 1
-add r2, r2, r0
+fadd r2, r2, r0
 mov r1, r0 << 1
-add r2, r2, r1
+fadd r2, r2, r1
 mov r0, r1 << 1
-add r2, r2, r0
+fadd r2, r2, r0
 mov r1, r0 << 1
-add r2, r2, r1
+fadd r2, r2, r1
 mov r0, r1 << 1
-add r2, r2, r0
+fadd r2, r2, r0
 mov r1, r0 << 1
-add r2, r2, r1
+fadd r2, r2, r1
 mov r0, r1 << 1
-add r2, r2, r0
+fadd r2, r2, r0
 mov r1, r0 << 1
-add r2, r2, r1
+fadd r2, r2, r1
 mov r0, r1 << 1
-add r2, r2, r0
+fadd r2, r2, r0
 mov r1, r0 << 1
-add r2, r2, r1
+fadd r2, r2, r1
 mov r0, r1 << 1
-add r2, r2, r0
+fadd r2, r2, r0
 mov r1, r0 << 1
-add r2, r2, r1
+fadd r2, r2, r1
 mov r0, r1 << 1
-add r2, r2, r0
+fadd r2, r2, r0
 mov r1, r0 << 1
-add r2, r2, r1
+fadd r2, r2, r1
 mov r0, r1 << 1
 
 # Configure the VPM for writing
